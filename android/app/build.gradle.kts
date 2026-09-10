5d1ebd1d66ea5166e10e1bd0a5a3479212ed8112
Joost <joost-vanandel@live.nl>
2026-09-10 12:33:30 +0000

    Suppress EGR findings during electric drive

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.obdvis.android"
         minSdk = 23
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.1.11"
+        versionCode = 13
+        versionName = "0.1.12"
     }
 
     buildTypes {
